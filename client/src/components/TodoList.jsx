64fd7845190a730f0c8c8cef30ef0ac445a52e15
Hrithik-C-A <hrithikca@gmail.com>
2023-04-16 18:55:31 +0530

    made app responsive

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -16,7 +16,7 @@ const TodoList = () => {
     const [editMode,setEditMode]= useState(false)
     const [editTodo,setEditTodo] = useState(null)
     const buttonTitle = editMode ? (<div className='flex gap-1'><BiEdit size={35}/> 
-    <TiCancelOutline onClick={()=>{
+    <TiCancelOutline className='text-orange-400' onClick={()=>{
         setTodoText('')
         setEditMode(false)
         setEditTodo(null)}} size={38}/></div>) : <AiOutlinePlusSquare size={40}/>
@@ -30,7 +30,6 @@ const TodoList = () => {
 
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        console.log('submitted')
         if (!todoText.trim()) {
           return;
         }
@@ -43,7 +42,6 @@ const TodoList = () => {
         else{
           const newTodo = {id: uuidv4(),todos: todoText}
           await axios.post(`${endpoint}/post`,[newTodo])
-          console.log(newTodo)
           dispatch({type:'add',payload:newTodo})
         }
         setTodoText('')
@@ -51,21 +49,22 @@ const TodoList = () => {
       
   return (
     <div>
-        <h1 className='font-bold text-4xl text-slate-700 text-center'>Mern Todo App</h1>
+        <h1 className='font-bold text-4xl text-slate-700 w text-center max-md:text-2xl'>Mern Todo App</h1>
 
-        <form className='flex gap-2 justify-center items-center mt-3' onSubmit={handleSubmit}>
+        <form className='flex gap-2 justify-center items-center mt-3 max-sm:flex-col ' onSubmit={handleSubmit}>
             <input className='bg-white  border-b-orange-400 border-b-4 text-black rounded-md py-1 px-2 focus:border-orange-500' type="text" value={todoText} name="todos" placeholder='Enter Your Task Here' onChange={(e)=>setTodoText(e.target.value)}  />
             <button className='text-slate-700' type="submit">{buttonTitle}</button>
         </form>
 
         <div>
-        <ul className='flex flex-col justify-center items-center mt-2'>
+        <ul className='flex flex-col justify-center items-center mt-2 max-sm:w-[80%] max-md:m-auto '>
             {state.todoList.map((item)=>{
-                return <li className='flex flex-row justify-between text-white bg-slate-700 w-4/12 m-2 p-2 rounded-lg font-medium ' key={item.id}>
+                return <li className='flex flex-row justify-between text-white bg-slate-700 w-5/12 m-2 p-2 rounded-lg font-medium  max-md:flex-col max-md:w-[70%] max-md:m-auto ' key={item.id}>
                         
-                            <p className='font-semibold my-auto'>{item.todos}</p>
+                        <p className='font-semibold my-auto max-md:block'>{item.todos}</p>
+                        
 
-                        <div className='flex gap-2'>
+                        <div className='flex gap-4'>
                                 <button className=' py-1  rounded-md' 
                                 onClick={()=>{
                                 setTodoText(item.todos)
@@ -80,7 +79,7 @@ const TodoList = () => {
                                 await axios.delete(`${endpoint}/delete/${item.id}`)
                                 dispatch({type:'delete',payload:{id: item.id}})
                                 }}>
-                                <MdDeleteOutline size={20}/>
+                                <MdDeleteOutline className='text-red-500' size={20}/>
                             </button>
                         </div>
                         
